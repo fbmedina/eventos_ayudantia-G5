adecc9cc3c77bfb4cfa10587894a82f6ac55fdf9
Francisca Medina Concha <fbmedina@uc.cl>
2017-12-01 01:27:36 -0300

    validations

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,12 @@
 class Calendar < ApplicationRecord
+	require 'Date'
+	validates :name, presence: true, uniqueness: true
+	validates :genre, presence: true
+	validate :cant_be_in_the_past
+
+	def cant_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "No puede ser en el pasado")
+    end
+  end
 end
